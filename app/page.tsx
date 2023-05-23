2fbe657ab52826a97feb59a60496d31c4c691d6d
Joel <joelkkusi@gmail.com>
2023-05-23 21:48:43 +0200

    Extra Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import { RedirectToSignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+    RedirectToSignIn,
+    SignedIn,
+    SignedOut,
+    UserButton,
+    UserProfile,
+} from "@clerk/nextjs";
 
 export default function Home() {
-
     return (
         <body className="bg-gradient-to-r from-gray-700 via-gray-900 to-black">
             <div>
@@ -9,12 +14,21 @@ export default function Home() {
                     <RedirectToSignIn />
                 </SignedOut>
                 <SignedIn>
-                    <div className="ml-6 mt-6">
-                        <UserButton afterSignOutUrl="/" />
+                    <div className="flex justify-around">
+                        <div className="">
+                            <UserButton />
+                        </div>
+                        <div>
+                            <UserProfile />
+                        </div>
+                        <div>
+                            <button className="rounded-full ...">
+                                Sign Out
+                            </button>
+                        </div>
                     </div>
                 </SignedIn>
             </div>
         </body>
-
     );
 }
--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,10 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-        <body className="bg-gradient-to-r from-gray-700 via-gray-900 to-black">
-            <div className="grid place-items-center h-screen">
-                <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-            </div>
-        </body>
+        <div className="grid place-items-center h-screen">
+            <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        </div>
     );
 }
